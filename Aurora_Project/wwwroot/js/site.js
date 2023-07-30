ba44ba5a1eaf02bb6b66845bba95099cd5bd2fdd
Salah Matini <salahmatini123@gmail.com>
2023-07-30 06:25:08 +0300

    Finished Navbar Finished Footer Edited Hero

--- Aurora_Project/wwwroot/js/site.js
+++ Aurora_Project/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.getElementById('navbar');
+    const scrollOffset = 100; // Adjust this value to control when the background color changes
+
+    function updateNavbarColor() {
+        if (window.scrollY >= scrollOffset) {
+            navbar.classList.add('black');
+        } else {
+            navbar.classList.remove('black');
+        }
+    }
+
+    // Call the function initially to set the initial state of the navbar
+    updateNavbarColor();
+
+    // Attach the function to the 'scroll' event to update the navbar while scrolling
+    window.addEventListener('scroll', updateNavbarColor);
+});
